2366020e4babbbee44ad241a3c884347c7323bc6
Theunkillabledemonking <abcqkdnxm@g.yju.com>
2024-08-19 16:03:09 +0900

    commit!

--- player.py
+++ player.py
@@ -16,33 +16,38 @@ class Player:
         self.rotation_angle = 0  # 회전 각도 초기화
         self.is_jumping = False  # 점프 상태 플래그
         self.rotation_steps = 0  # 회전 단계 초기화
+        self.space_pressed = False
 
     def handle_movement(self):
         """플레이어의 움직임 및 물리 엔진 처리"""
         keys = pygame.key.get_pressed()
         
-        if keys[pygame.K_SPACE] and not self.is_jumping :
+        if keys[pygame.K_SPACE] and not self.is_jumping and not self.space_pressed :
             self.is_jumping = True
             self.gravity = -7  # 점프 시 중력 설정
             self.rotation_steps = 30  # 30번 회전 (6도씩 회전하여 180도 회전)
-            
+            self.space_pressed = True
+            print(self.rotation_steps)
+            print(self.rotation_angle)
 
+        if not keys[pygame.K_SPACE]:
+            self.space_pressed = False # 스페이스바가 다시 눌릴 수 있도록 설정
+            
         # 중력 적용
         self.rect.y += self.gravity
         if self.rect.bottom >= screen_height:
             self.rect.bottom = screen_height  # 바닥에 닿으면 위치 고정
             self.gravity = 0  # 중력 초기화
             self.is_jumping = False  # 바닥에 닿으면 점프 상태 해제
+
         else:
             self.gravity += 0.4  # 중력 가속도
 
         # 회전 처리
-        if self.is_jumping:
-            if self.rotation_steps > 0 :
-                self.rotation_angle -= 6 # 시계 방향으로 6도씩 회전
-                self.rotation_steps -= 1
-            else:
-                self.is_jumping = False
+        if self.is_jumping and self.rotation_steps >0 :
+            self.rotation_angle -= 6 # 시계 방향으로 6도씩 회전
+            self.rotation_steps -= 1
+            
 
     def get_rotated_image(self):
         """현재 회전된 이미지와 그 위치를 반환"""
